9514f0c5dd5a2c1b742361bf6e7f664b8f4cf090
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-25 15:43:33 -0700

    Delete delete_file.lua

--- delete_file.lua
+++
@@ -1,6 +0,0 @@
-function main()
-	local fileToDelete = string.gsub(mp.get_property("path"), "/", "\\")
-	mp.commandv('run','cmd.exe', '/C', 'del', fileToDelete)
-	mp.commandv('playlist-remove','current')
-end
-mp.register_script_message("delete-file", main)
