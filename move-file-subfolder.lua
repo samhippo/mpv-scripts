aa82b866a069f639b62b974c66fd409d6d901841
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-27 09:16:25 -0700

    Update move-file-subfolder.lua

--- move-file-subfolder.lua
+++ move-file-subfolder.lua
@@ -3,7 +3,9 @@ function main(subfolder)
     local thisFolder, _ = utils.split_path(string.gsub(mp.get_property("path"), "/", "\\"))
     local destFolder = utils.join_path(thisFolder, subfolder)
     local fileToMove = string.gsub(mp.get_property("path"), "/", "\\")
-    mp.commandv('run','cmd.exe', '/C', 'md', destFolder ,'&','move', fileToMove, destFolder)
+    local args = {'cmd.exe', '/C', 'md', destFolder ,'&','move', fileToMove, destFolder}
+	utils.subprocess({args = args, playback_only = false})
     mp.commandv('playlist-remove','current')
 end
 mp.register_script_message("move-file-subfolder", main)
+
