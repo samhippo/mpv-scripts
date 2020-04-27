b6a660c4d1377faae855019fa86bddbbbf51ac4f
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-27 09:27:05 -0700

    Update move-file.lua

--- move-file.lua
+++ move-file.lua
@@ -1,6 +1,8 @@
+local utils = require "mp.utils"
 function main(destFolder)
     local fileToMove = string.gsub(mp.get_property("path"), "/", "\\")
-    mp.commandv('run','cmd.exe', '/C', 'md', destFolder ,'&','move', fileToMove, destFolder)
+    local args = {'cmd.exe', '/C', 'md', destFolder ,'&','move', fileToMove, destFolder}
+	utils.subprocess({args = args, playback_only = false})
 	mp.commandv('playlist-remove','current')
 end
 mp.register_script_message("move-file", main)
