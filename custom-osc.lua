275ed27615e0f6178dda7860e5a9a67a6f8844ac
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-27 14:39:06 -0700

    Add files via upload

---
+++ custom-osc.lua
@@ -0,0 +1,48 @@
+function leftClick()
+    mouseEvent("leftClick")
+end
+
+function leftDoubleClick()
+    mouseEvent("leftDoubleClick")
+end
+
+function mouseEvent(event)
+	local x, y = mp.get_mouse_pos()
+	local window_width = mp.get_property("osd-width")
+	local window_height = mp.get_property("osd-height")
+	local bottomArea = window_height * .8
+	local leftArea = window_width * .2
+	local rightArea = window_width * .8
+	local seek_perc = (x / window_width) * 100
+
+	if(y > bottomArea) then
+		mp.commandv('osd-msg-bar','seek',seek_perc, 'absolute-percent')
+	elseif(x < leftArea and y < bottomArea) then
+		mp.commandv('playlist-prev')
+	elseif(x > rightArea and y < bottomArea) then
+		mp.commandv('playlist-next')
+    else
+         if(event == "leftClick") then
+            mp.commandv('cycle','pause')
+         elseif(event == "leftDoubleClick") then
+            mp.msg.log('fatal',"trying to fullscreen")
+            mp.commandv('cycle','fullscreen')
+         end
+	end
+end
+
+function mouseMove()
+	local x, y = mp.get_mouse_pos()
+	local width = mp.get_property("osd-width")
+	local height = mp.get_property("osd-height")
+	local seekArea = height * .8
+	if(y > seekArea) then
+		mp.commandv('osd-msg-bar','show-progress')
+	end
+end
+
+mp.register_script_message("custom-osc-left-click", leftClick)
+mp.register_script_message("custom-osc-left-double-click", leftDoubleClick)
+mp.register_script_message("custom-osc-right-click", rightClick)
+mp.register_script_message("custom-osc-mouse-move", mouseMove)
+
