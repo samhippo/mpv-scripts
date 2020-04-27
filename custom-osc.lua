1c3befcb6ffc8a33afb2e3f29327f68577c90759
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-27 16:50:31 -0700

    Add files via upload

--- custom-osc.lua
+++ custom-osc.lua
@@ -1,9 +1,22 @@
+local dragging = false
+local prevX = 0
+
 function leftClick()
     mouseEvent("leftClick")
 end
 
 function leftDoubleClick()
     mouseEvent("leftDoubleClick")
+end
+
+function dragSeek(kevent)
+    if(kevent["event"] == "down") then
+        local x, y = mp.get_mouse_pos()
+        prevX = x
+        dragging = true
+    else
+        dragging = false
+    end
 end
 
 function mouseEvent(event)
@@ -37,9 +50,18 @@ function mouseMove()
 	local seekArea = height * .8
 	if(y > seekArea) then
 		mp.commandv('osd-msg-bar','show-progress')
-	end
+    end
+    
+    if(dragging == true) then
+        local x, y = mp.get_mouse_pos()
+        local diff = (prevX - x) * .5;
+        mp.commandv('osd-msg-bar','seek',diff)
+        prevX = x
+    end
 end
 
+mp.add_forced_key_binding("MBTN_RIGHT", "dragSeek", dragSeek, { repeatable = false; complex = true })
+mp.register_script_message("custom-osc-right-click", rightClick)
 mp.register_script_message("custom-osc-left-click", leftClick)
 mp.register_script_message("custom-osc-left-double-click", leftDoubleClick)
 mp.register_script_message("custom-osc-mouse-move", mouseMove)
