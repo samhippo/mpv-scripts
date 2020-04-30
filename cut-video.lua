5a132e34beaed3f20de7e87a8c70c7780d8440b0
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-29 18:13:49 -0700

    Update cut-video.lua

--- cut-video.lua
+++ cut-video.lua
@@ -5,23 +5,34 @@ local end_time_formated = nil
 local utils = require "mp.utils"
 local ov = mp.create_osd_overlay("ass-events")
 
-function fn_cut_finish(output_format)
+function fn_cut_finish(p1,p2)
   if(start_time_seconds == nil or end_time_seconds == nil) then
     mp.osd_message("time not set")
   else
-    local output_directory, _ = utils.split_path(mp.get_property("path"))
-    local output_ext = mp.get_property("filename"):match("[^.]+$")
-    if(output_format ~= nil and output_format ~= output_ext) then
-      local output_fname = mp.get_property("filename/no-ext").."_"..string.gsub(start_time_formated,":",".").."-"..string.gsub(end_time_formated,":",".").."."..output_format
-      local output = utils.join_path(output_directory, output_fname)
-      mp.msg.info("ffmpeg -ss "..start_time_seconds.." -i "..mp.get_property("path").." -to "..tostring(end_time_seconds-start_time_seconds).." "..output)
-      mp.commandv("run","ffmpeg","-ss",start_time_seconds,"-i",mp.get_property("path"),"-to",tostring(end_time_seconds-start_time_seconds),output)
+    local video_args = '-c copy'
+    local output_format =  mp.get_property("filename"):match("[^.]+$")
+    if((p1 == nil and p2 == nil) or (p1 == '' and p2 == '')) then
     else
-      local output_fname = mp.get_property("filename/no-ext").."_"..string.gsub(start_time_formated,":",".").."-"..string.gsub(end_time_formated,":",".").."."..output_ext
-      local output = utils.join_path(output_directory, output_fname)
-      mp.msg.info("ffmpeg -ss "..start_time_seconds.." -i "..mp.get_property("path").." -to "..tostring(end_time_seconds-start_time_seconds).." -c copy "..output)
-      mp.commandv("run","ffmpeg","-ss",start_time_seconds,"-i",mp.get_property("path"),"-to",tostring(end_time_seconds-start_time_seconds),"-c","copy",output) 
+      if(p1 == nil or p1 == '') then
+      else
+        output_format = p1
+      end
+      if(p2 == nil or p2 == '') then
+        video_args = ''
+      else
+        video_args = p2
+      end
     end
+    local output_directory, _ = utils.split_path(mp.get_property("path"))
+    local output_filename = mp.get_property("filename/no-ext").."_"..string.gsub(start_time_formated,":",".").."-"..string.gsub(end_time_formated,":",".").."."..output_format
+    local output_path = utils.join_path(output_directory, output_filename)
+    local args = {'ffmpeg', '-ss', tostring(start_time_seconds), "-i",tostring(mp.get_property("path")),"-to",tostring(end_time_seconds-start_time_seconds)}
+    for token in string.gmatch(video_args, "[^%s]+") do
+      mp.msg.fatal(token)
+      table.insert(args,token)
+    end
+    table.insert(args,output_path)
+    utils.subprocess_detached({args = args, playback_only = false})
     start_time_formated = nil
     start_time_seconds = nil
     end_time_seconds = nil
