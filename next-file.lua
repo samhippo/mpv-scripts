bf3c04042dc2137b3dda1b6ad0f61abf95d3e725
samhippo <63636707+samhippo@users.noreply.github.com>
2020-05-12 16:54:46 -0700

    Add files via upload

--- next-file.lua
+++ next-file.lua
@@ -1,6 +1,5 @@
 local utils = require "mp.utils"
 
-
 local ext = { 
     "mkv","mp4","webm","wmv","avi","3gp","ogv","mpg","mpeg","mov","vob" ,"ts","m2ts","divx","flv","asf","m4v","h264","h265","rmvb","rm","ogm"
     ,"jpg","jpeg","bmp","gif","png","svg","tif","tiff"
@@ -12,29 +11,31 @@ for i = 1, #ext do
     valid[ext[i]] = true
 end
 
-
-local function isNew(filename)
-    local playlist = mp.get_property_native('playlist')
-    for i = 1, #playlist do
-        if (playlist[i].filename == filename) then
-            return false
-        end
-    end
-    return true
-end
-
-local function main()
+local function getFileList()
     local dir, f = utils.split_path(mp.get_property("path"))
     local ar = utils.readdir(dir,"files")
+    local fileList = {}
     for i, name in ipairs(ar) do
         local ext = string.match(name, "%.([^.]+)$")
         if(ext == nil) then ext = '' end
         ext = string.lower(ext)
-        local filename = utils.join_path(dir, name)
-        if(valid[ext] and isNew(filename)) then
-            mp.commandv('loadfile',filename,'append')
+        if(valid[ext]) then
+            table.insert(fileList,name)
+        end
+    end
+    return fileList
+end
+
+local function main()
+    local fileList = getFileList()
+    for i = 1, #fileList do
+        if(mp.get_property("filename") == fileList[i]) then
+            local next = i + 1
+            if(next > #fileList) then next = 1 end
+            local dir, f = utils.split_path(mp.get_property("path"))
+            mp.commandv('loadfile',utils.join_path(dir, fileList[next]),'replace')
+            return
         end
     end
 end
-
-mp.register_event("file-loaded",main)
+mp.register_script_message("next-file", main)
